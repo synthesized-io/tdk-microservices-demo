76767de64ad13978a89712d4f1733e9f6b72beee
yuri <4427322+yruchin@users.noreply.github.com>
2023-03-23 20:49:11 +0000

    The actual invocation

---
+++ films/src/main/java/io/synthesized/microservices/films/service/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package io.synthesized.microservices.films.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
