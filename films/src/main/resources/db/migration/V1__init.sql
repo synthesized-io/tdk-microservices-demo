4c38b78173c1a0978c0232a4dde4c8385cb80636
vitaly <vitaly@synthesized.io>
2023-04-26 19:46:49 +0400

    Fix scripts

--- films/src/main/resources/db/migration/V1__init.sql
+++ films/src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e838b63716c51aa57eb8df559fbcb7ef8f0bf4c2eb6a439cc11634debc5901d
-size 567883
+oid sha256:c2a6323ca5868e6f8a6f5ffbf40a0548a7ed0e9b94aaf465d510e3f8f49884dc
+size 567611
--- payments/src/main/resources/db/migration/V1__init.sql
+++ payments/src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed5ec07819e74081777fa73868f28d808aea53a07d731d775a866c747212510b
-size 2524167
+oid sha256:e17f4cd65e1de9bb2533d30cbd52be13fbed7c262c4dfa2719766e4e457b0871
+size 2523895
